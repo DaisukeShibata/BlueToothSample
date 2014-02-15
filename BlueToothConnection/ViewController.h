d033252189883a7e722f8c2c50ffb1cb25169ae1
Daisuke Shibata <d.shibata0818@gmail.com>
2014-02-15 17:52:05 +0900

    bluetooth通信

--- BlueToothConnection/ViewController.h
+++ BlueToothConnection/ViewController.h
@@ -1,13 +1,30 @@
 //
 //  ViewController.h
-//  BlueToothConnection
+//  BlueToothConnect
 //
-//  Created by Daisuke Shibata on 2014/02/15.
-//  Copyright (c) 2014年 Daisuke Shibata. All rights reserved.
+//  Created by 柴田　大介 on 2014/02/15.
+//  Copyright (c) 2014年 dshibata. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<MCNearbyServiceAdvertiserDelegate, MCNearbyServiceBrowserDelegate, MCSessionDelegate>
+{
+    MCPeerID *myPeerID;
+    NSString *serviceType;
+}
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (strong, nonatomic) MCPeerID *myPeerID;
+@property (strong, nonatomic) NSString *serviceType;
+@property (strong, nonatomic) MCNearbyServiceAdvertiser *nearbyServiceAdvertiser;
+@property (strong, nonatomic) MCNearbyServiceBrowser *nearbyServiceBrowser;
+@property (strong, nonatomic) MCSession *session;
+
+@property (nonatomic) NSArray *sendArray;
+
+- (IBAction)btnStartAdvertisingIPHONE:(id)sender;
+- (IBAction)btnStopAdvertisingIPHONE:(id)sender;
 
 @end
